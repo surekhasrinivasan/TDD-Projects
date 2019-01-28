4fefa6afe9889d5638101d59c599849ddc50aede
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-28 12:17:30 -0500

    TDD - CheckPassword - initial commit

---
+++ CheckPassword/CheckPasswordTests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPasswordTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        
+        [Test]
+        public void ThrowExceptionIfpasswordIsLessThan6Chars()
+        {
+            
+        }
+    }
+}
---
+++ CheckPassword/PasswordService/Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckPasswordService;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Password pw = new Password();
+
+        [Test]
+        public void ThrowExceptionIfPasswordLengthIsLessThan6Chars()
+        {
+            var input = "alpha";
+            Assert.Throws<System.ArgumentException>(() => pw.CheckPassword(input));
+        }    
+    }
+}
