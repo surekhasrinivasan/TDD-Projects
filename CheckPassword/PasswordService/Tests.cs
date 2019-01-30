fd095e0f6fc985654e8a521af9e6b943ca893973
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-29 23:14:37 -0500

    All the tests to CheckPassword

--- CheckPassword/PasswordService/Tests.cs
+++ CheckPassword/PasswordService/Tests.cs
@@ -10,14 +10,112 @@ namespace Tests
 {
     [TestFixture]
     public class Tests
-    {
-        Password pw = new Password();
-
+    {       
         [Test]
-        public void ThrowExceptionIfPasswordLengthIsLessThan6Chars()
+        public void PasswordLengthShouldNotBeLessThan6Chars()
         {
             var input = "alpha";
-            Assert.Throws<System.ArgumentException>(() => pw.CheckPassword(input));
-        }    
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Length of the Password string cannot be less than 6 characters");
+        }
+
+
+        [Test]
+        public void PasswordLengthShouldNotBeMoreThan12Chars()
+        {
+            var input = "passwordalpha";
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Length of the Password string cannot be more than 12 characters");
+        }
+
+
+        [Test]
+        public void PasswordShouldHaveNoWhiteSpace()
+        {
+            var input = "alPhas ";
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Password string cannot have white space");
+        }
+
+        [Test]
+        public void PasswordShouldHave1UpperCaseChar()
+        {
+            var input = "zebras";
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Password should contain one upper case character");
+        }
+
+        [Test]
+        public void PasswordShouldHave1LowerCaseChar()
+        {
+            var input = "CACTUS";
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Password should contain one lower case character");
+        }
+
+        [Test]
+        public void PasswordShouldNotHaveTwoSimilarCharactersConsecutively()
+        {
+            var input = "Tomorrow";
+            bool expected = false;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Password string cannot have two similar characters consecutively");
+        }
+
+        [Test]
+        public void PasswordShouldHave1SpecialChar()
+        {
+            var input = "$Smithonian";
+            bool expected = true;
+            bool actual = Password.CheckPassword(input);
+            bool test;
+
+            if (expected.Equals(actual))
+                test = true;
+            else
+                test = false;
+            Assert.IsTrue(test, "Password Should have 1 Special character");            
+        }
     }
 }
