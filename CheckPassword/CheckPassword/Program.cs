92e65c55da0df6b6fa8ab514ce089cd311e77b32
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-29 23:09:36 -0500

    Program.cs is console app which prints the boolean value returned by the function CheckPassword

---
+++ CheckPassword/CheckPassword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckPasswordService;
+
+namespace CheckPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Password.CheckPassword("$Alphes"));
+
+            Console.WriteLine(Password.CheckPassword("@John123"));
+
+            Console.WriteLine(Password.CheckPassword("Sami&123"));
+
+            Console.WriteLine(Password.CheckPassword("Aum Chant"));
+
+            Console.WriteLine(Password.CheckPassword("abracaDabrabra"));
+
+            bool result = Password.CheckPassword("Smith");
+            Console.WriteLine(result);
+        }
+    }
+}
