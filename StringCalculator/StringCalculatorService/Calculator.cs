59d7b615b65df74a4ea60fee74cb050745b56f35
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-31 13:58:32 -0500

    String Calculator - 4 tests passed

---
+++ StringCalculator/StringCalculatorService/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorService
+{
+    public static class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            if (numbers == "")
+                return 0;
+
+            if (numbers.Length == 1)
+            {
+                int num = int.Parse(numbers);
+                return num;
+            }
+            int sum = 0;
+            string[] strNum = numbers.Split(',');
+            
+            for(int i = 0; i < strNum.Length; i++)
+            {
+                sum +=int.Parse(strNum[i]);
+            }
+            
+            return sum;          
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/Tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using StringCalculatorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void IfInputIsEmptyStringReturnZero()
+        {
+            var input = "";
+            var expected = 0;
+            var actual = Calculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfInputIsOneNumberReturnThatNumber()
+        {
+            var input = "1";
+            var expected = 1;
+            var actual = Calculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfInputIsTwoNumbersReturnTheirSum()
+        {
+            var input = "1, 2";
+            var expected = 3;
+            var actual = Calculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfInputIsMoreThanTwoNumbersReturnTheirSum()
+        {
+            var input = "5, 5, 5, 5";
+            var expected = 20;
+            var actual = Calculator.Add(input);
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
