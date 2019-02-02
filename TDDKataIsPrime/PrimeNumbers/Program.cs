208614fa46da05f17d05af5890cb2636bfc05970
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-02 07:56:04 -0500

    TDD Kata - IsPrime - initial commit

---
+++ TDDKataIsPrime/PrimeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
