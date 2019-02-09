55d15ca3c66e2f828b33950bbfc71428da9b6eef
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-09 12:12:32 -0500

    Added a console program to print fibonacci numbers

---
+++ FibonacciSequenceSeries/FibonacciSequenceSeries/Program.cs
@@ -0,0 +1,22 @@
+﻿using Fibonacci.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequenceSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new FibonacciSequence();
+            foreach (var item in sequence.Take(50))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
