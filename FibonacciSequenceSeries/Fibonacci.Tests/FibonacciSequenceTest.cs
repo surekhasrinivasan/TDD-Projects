fe736b4058cdc3f498eac8c5b4ed5506863e7377
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-09 22:06:42 -0500

    12 tests passed

--- FibonacciSequenceSeries/Fibonacci.Tests/FibonacciSequenceTest.cs
+++ FibonacciSequenceSeries/Fibonacci.Tests/FibonacciSequenceTest.cs
@@ -13,28 +13,47 @@ namespace Fibonacci.Tests
     [TestFixture]
     public class FibonacciSequenceTest
     {
-        [Test]
-        public void FibonacciSequence_FirstElement_IsOne()
-        {
-            var seq = new FibonacciSequence();
-            var result = seq.First();
-            Assert.AreEqual(1, result);
-        }
+        //[Test]
+        //public void FibonacciSequence_FirstElement_IsOne()
+        //{
+        //    var seq = new FibonacciSequence();
+        //    var result = seq.First();
+        //    Assert.AreEqual(1, result);
+        //}
 
-        [Test]
-        public void FibonacciSequence_SecondElement_IsOne()
-        {
-            var seq = new FibonacciSequence();
-            var result = seq.ElementAt(1);
-            Assert.AreEqual(1, result);
-        }
+        //[Test]
+        //public void FibonacciSequence_SecondElement_IsOne()
+        //{
+        //    var seq = new FibonacciSequence();
+        //    var result = seq.ElementAt(1);
+        //    Assert.AreEqual(1, result);
+        //}
 
-        [Test]
-        public void FibonacciSequence_ThirdElement_IsTwo()
+        //[Test]
+        //public void FibonacciSequence_ThirdElement_IsTwo()
+        //{
+        //    var seq = new FibonacciSequence();
+        //    var result = seq.ElementAt(2);
+        //    Assert.AreEqual(2, result);
+        //}
+
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 5)]
+        [TestCase(5, 8)]
+        [TestCase(6, 13)]
+        [TestCase(7, 21)]
+        [TestCase(8, 34)]
+        [TestCase(9, 55)]
+        [TestCase(10, 89)]
+        [TestCase(11, 144)]
+        public void FibonacciSequence_Element_IsValue(int n, int expected)
         {
             var seq = new FibonacciSequence();
-            var result = seq.ElementAt(2);
-            Assert.AreEqual(2, result);
+            var result = seq.ElementAt(n);
+            Assert.AreEqual(expected, result);
         }
     }
 }
