80d7c1545aeac5f0fc310b4e7702349b9291f3e8
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-10 21:41:43 -0500

    2 tests passed

--- ConwaysGameOfLife/Conway.Library.Tests/LifeRulesTests.cs
+++ ConwaysGameOfLife/Conway.Library.Tests/LifeRulesTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,18 @@ namespace Conway.Library.Tests
     // Any live cell with two or three live neighbors lives 
     // Any live cell with more than three live neighbors dies. 
     // Any dead cell with exactly three live neighbors becomes a live cell. 
-
+    
+    [TestFixture]
     public class LifeRulesTests
     {
+        [Test]
+        public void LiveCell_FewerThan2LiveNeighbors_Dies(
+           [Values(0, 1)] int liveNeighbors)
+        {
+            var currentState = CellState.Alive;
+            CellState newState = LifeRules.GetNewState(currentState, liveNeighbors);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
     }
 }
--- ConwaysGameOfLife/Conway.Library/LifeRules.cs
+++ ConwaysGameOfLife/Conway.Library/LifeRules.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Conway.Library
 {
+    public enum CellState
+    {
+        Alive,
+        Dead,
+    };
+
     public class LifeRules
     {
+        public static CellState GetNewState(CellState currentState, int liveNeighbors)
+        {
+            if (currentState == CellState.Alive && liveNeighbors < 2)
+                return CellState.Dead;            
+            return currentState;
+        }
     }
 }
