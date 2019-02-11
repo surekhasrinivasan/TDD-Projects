70abc90683995251f3e417b3af20f80bfbd1d3a9
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-10 21:26:43 -0500

    TDD - ConwaysGameOfLife - initial commit

---
+++ ConwaysGameOfLife/Conway.Library.Tests/LifeRulesTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conway.Library.Tests
+{
+    // Any live cell with fewer than two live neighbors dies.
+    // Any live cell with two or three live neighbors lives 
+    // Any live cell with more than three live neighbors dies. 
+    // Any dead cell with exactly three live neighbors becomes a live cell. 
+
+    public class LifeRulesTests
+    {
+    }
+}
