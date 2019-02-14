8abfbf432fb17ed02682c117b2501c7567029789
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-13 20:14:16 -0500

    Refactored code by adding Switch statement

--- ConwaysGameOfLife/Conway.Library/LifeRules.cs
+++ ConwaysGameOfLife/Conway.Library/LifeRules.cs
@@ -16,10 +16,23 @@ namespace Conway.Library
     {
         public static CellState GetNewState(CellState currentState, int liveNeighbors)
         {
-            if (currentState == CellState.Alive && liveNeighbors < 2 || liveNeighbors > 3)
-                return CellState.Dead;
-            if (currentState == CellState.Dead && liveNeighbors == 3)
-                return CellState.Alive;
+            //if (currentState == CellState.Alive && liveNeighbors < 2 || liveNeighbors > 3)
+            //    return CellState.Dead;
+            //if (currentState == CellState.Dead && liveNeighbors == 3)
+            //    return CellState.Alive;
+            //return currentState;
+
+            switch (currentState)
+            {
+                case CellState.Alive:
+                    if (liveNeighbors < 2 || liveNeighbors > 3)
+                        return CellState.Dead;
+                    break;
+                case CellState.Dead:
+                    if (liveNeighbors == 3)
+                        return CellState.Alive;
+                    break;
+            }
             return currentState;
         }
     }
